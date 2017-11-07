0b4adb52efa6052e390d116d8294b495011c4d6f
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-11-07 20:14:33 +0600

    complete assignment 4

---
+++ assignment4/reducer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+files = {}
+max_ = 0
+
+for line in sys.stdin:
+    if line in files:
+	files[line] = files[line]+1
+    else:
+	files[line] = 1
+
+for item in files:
+    if files[item] > max_:
+	path = item
+	max_ = files[item]
+print(max_)
